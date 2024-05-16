5f6a7cb7dd6b5c198830922a0257da33a77867b1
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-05-16 14:27:02 +0900

    2024/05/16 오전 커밋

---
+++ daeut/src/main/java/com/daeut/daeut/DaeutApplication.java
@@ -0,0 +1,13 @@
+package com.daeut.daeut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeutApplication.class, args);
+	}
+
+}
---
+++ daeut/src/test/java/com/daeut/daeut/DaeutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeut.daeut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
