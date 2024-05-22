ca9e739800313bf256eb08cbaf8efecd1b976746
jongsiklee01 <sononot445@gmail.com>
2024-05-22 20:58:15 +0900

    24.05.22 인덱스 슬라이드 위치 수정, 전체 위치 수정

---
+++ daeut/src/main/java/com/daeut/daeut/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.daeut.daeut.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+
+
+        http.authorizeRequests(requests -> requests.antMatchers("/**").permitAll()
+                                                    .anyRequest().authenticated()
+                                                    );
+
+        return http.build();
+    }
+
+	
+    
+}
--- daeut/src/main/java/com/daeut/daeut/controller/HomeController.java
+++ daeut/src/main/java/com/daeut/daeut/controller/HomeController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("")
 public class HomeController {
 
-    @GetMapping("/index")
+    @GetMapping("")
     public String index() {
         return "/index";
-    }
-    
+    }   
 }
