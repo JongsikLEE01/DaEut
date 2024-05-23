b5b5e1b24824591fd657c6cc7425a649579dd617
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-05-23 13:16:43 +0900

    5.23 백업본

--- daeut/src/main/resources/static/js/indexScript.js
+++ daeut/src/main/resources/static/js/indexScript.js
@@ -47,58 +47,97 @@ window.addEventListener('DOMContentLoaded', event => {
 // 날씨
 // 날씨 api - fontawesome 아이콘 사용
 var weatherIcon = {
-    '01' : 'fas fa-sun',
-    '02' : 'fas fa-cloud-sun',
-    '03' : 'fas fa-cloud',
-    '04' : 'fas fa-cloud-meatball',
-    '09' : 'fas fa-cloud-sun-rain',
-    '10' : 'fas fa-cloud-showers-heavy',
-    '11' : 'fas fa-poo-storm',
-    '13' : 'far fa-snowflake',
-    '50' : 'fas fa-smog'
-};
+    '01' : 'fa-solid fa-sun',
+    '02' : 'fa-solid fa-cloud-sun',
+    '03' : 'fa-solid fa-cloud',
+    '04' : 'fa-solid fa-cloud-meatball',
+    '09' : 'fa-solid fa-cloud-sun-rain',
+    '10' : 'fa-solid fa-cloud-showers-heavy',
+    '11' : 'fa-solid fa-cloud-bolt',
+    '13' : 'fa-solid fa-snowflake',
+    '50' : 'fa-solid fa-smog'
+}
 
-// 날씨 api - 서울
+// 날씨 api
+// 사용자의 위치를 가져오는 함수
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                weatherData(latitude, longitude)
+            },
+            error => {
+                console.error('위치 정보 로드 중 에러 발생 : ', error)
+            }
+        )
+    } else {
+        console.error('해당 브라우저는 위치 정보를 가져올 수 없습니다.')
+    }
+}
+
+// 페이지 로드 시 함수 실행
+window.addEventListener('load', getLocation);
 
 // api key
-const API_KEY = 'ef8952bfbab9356b5066de2f01ab56c1'
-// 사용자 위치 값
-const lat = position.coords.latitude
-const lon = position.coords.longitude
-var apiURI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+// 위치 정보를 바탕으로 날씨 데이터를 가져오는 함수
+function weatherData(latitude, longitude) {
+    const API_KEY = 'ef8952bfbab9356b5066de2f01ab56c1'
+    const apiURI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=kr`;
 
-$.ajax({
-    url: apiURI,
-    dataType: "json",
-    type: "GET",
-    async: "false",
-    success: function(resp) {
-        // json 데이터 가져오기
-        var $Icon = (resp.weather[0].icon).substr(0,2);
-        var $weather_description = resp.weather[0].main;
-        var $Temp = Math.floor(resp.main.temp- 273.15) + 'º';
-        var $humidity = '습도&nbsp;&nbsp;&nbsp;&nbsp;' + resp.main.humidity+ ' %';
-        var $wind = '바람&nbsp;&nbsp;&nbsp;&nbsp;' +resp.wind.speed + ' m/s';
-        var $city = '서울';
-        var $cloud = '구름&nbsp;&nbsp;&nbsp;&nbsp;' + resp.clouds.all +"%";
-        var $temp_min = '최저 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_min- 273.15) + 'º';
-        var $temp_max = '최고 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_max- 273.15) + 'º';
-        
-        // 가져온 데이터 화면에 출력
-        $('.weather_icon').append('<i class="' + weatherIcon[$Icon] +' fa-5x" style="height : 130px; width : 130px;"></i>');
-        $('.weather_description').prepend($weather_description);
-        $('.current_temp').prepend($Temp);
-        $('.humidity').prepend($humidity);
-        $('.wind').prepend($wind);
-        $('.city').append($city);
-        $('.cloud').append($cloud);
-        $('.temp_min').append($temp_min);
-        $('.temp_max').append($temp_max);               
-    }
+    $.ajax({
+        url: apiURI,
+        dataType: "json",
+        type: "GET",
+        async: false,
+
+        // 성공
+        success: function(response) {
+            console.log( response)
+
+            // JSON 데이터 가져오기
+            var $weather_description =  response.weather[0].description
+            var $Temp = Math.floor( response.main.temp) + 'º'
+            var $humidity = '습도 : ' +  response.main.humidity + ' %'
+            var $wind = '바람 : ' +  response.wind.speed + ' m/s'
+            var $city =  response.name
+            var $cloud = '구름 : ' +  response.clouds.all + "%"
+            var $temp_min = '최저 온도 : ' + Math.floor( response.main.temp_min) + 'º'
+            var $temp_max = '최고 온도 : ' + Math.floor( response.main.temp_max) + 'º'
+            var $Icon = ( response.weather[0].icon).substr(0,2)
+            
+            console.log($Icon)
+            var iconClass = weatherIcon[$Icon];
+            console.log(iconClass)
+
+            // 가져온 데이터 화면에 출력
+            $('.weather_icon').html('<i class="' + iconClass + '"></i>');
+            $('.weather_description').text($weather_description);
+            $('.current_temp').text($Temp)
+            $('.humidity').text($humidity)
+            $('.wind').text($wind)
+            $('.city').text($city)
+            $('.cloud').text($cloud)
+            $('.temp_min').text($temp_min)
+            $('.temp_max').text($temp_max)
+        },
+        // 실패
+        error: function(error) {
+            console.error('날씨 데이터 로드 중 오류 발생 :', error)
+        }
+    })
+}
+
+// 페이지 로드 시 사용자의 위치 기반으로 날씨 데이터 가져오기
+$(document).ready(function() {
+    getLocation();
 })
 
 
-// 버튼박스
+
+
+// 버튼 박스
 function showService(option) {
     // 모든 출력 요소를 숨김
     document.querySelectorAll('.output').forEach(function(el) {
