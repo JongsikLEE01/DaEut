d074eabebdfd0744afbe7f1b8134e7fd61772cdc
djhwanndj <dodgehwang@gmail.com>
2024-05-24 18:19:15 +0900

    Readpage 수정

--- daeut/src/main/resources/static/js/reservation/reservation.js
+++ daeut/src/main/resources/static/js/reservation/reservation.js
@@ -123,3 +123,22 @@ tagButtons.forEach(function(button) {
         button.classList.toggle('selected');
     });
 });
+
+// 이미지 슬라이드 
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("reservation-image-slide");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
