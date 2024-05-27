9f283d889af10c324e7f98a0d3c52bdb680d43b5
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-05-27 10:31:27 +0900

    05.27 인덱스 애니메이션 추가

--- daeut/src/main/resources/static/js/scripts.js
+++ daeut/src/main/resources/static/js/scripts.js
@@ -136,7 +136,6 @@ $(document).ready(function() {
 
 
 
-
 // 버튼 박스
 function showService(option) {
     // 모든 출력 요소를 숨김
@@ -146,3 +145,36 @@ function showService(option) {
     // 해당하는 출력 요소를 표시
     document.getElementById('output' + option.charAt(option.length - 1)).style.display = 'block';
 }
+
+
+// 스크롤 애니메이션
+$(document).ready(function() {
+    // 클래스가 "scroll_on"인 모든 요소를 선택합니다.
+    const $counters = $(".scroll_on");
+    
+    // 노출 비율(%)과 애니메이션 반복 여부(true/false)를 설정합니다.
+    const exposurePercentage = 100; // ex) 스크롤 했을 때 $counters 컨텐츠가 화면에 100% 노출되면 숫자가 올라갑니다.
+    const loop = true; // 애니메이션 반복 여부를 설정합니다. (true로 설정할 경우, 요소가 화면에서 사라질 때 다시 숨겨집니다.)
+
+    // 윈도우의 스크롤 이벤트를 모니터링합니다.
+    $(window).on('scroll', function() {
+        // 각 "scroll_on" 클래스를 가진 요소에 대해 반복합니다.
+        $counters.each(function() {
+            const $el = $(this);
+    
+            // 요소의 위치 정보를 가져옵니다.
+            const rect = $el[0].getBoundingClientRect();
+            const winHeight = window.innerHeight; // 현재 브라우저 창의 높이
+            const contentHeight = rect.bottom - rect.top; // 요소의 높이
+            
+            // 요소가 화면에 특정 비율만큼 노출될 때 처리합니다.
+            if (rect.top <= winHeight - (contentHeight * exposurePercentage / 100) && rect.bottom >= (contentHeight * exposurePercentage / 100)) {
+                $el.addClass('active');
+            }
+            // 요소가 화면에서 완전히 사라졌을 때 처리합니다.
+            if (loop && (rect.bottom <= 0 || rect.top >= window.innerHeight)) {
+                $el.removeClass('active');
+            }
+        });
+    }).scroll();
+});
