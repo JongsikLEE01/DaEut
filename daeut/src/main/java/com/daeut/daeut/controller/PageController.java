1ba2d79c3ff98edda3fbe70e868914d797f341b0
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-05-28 17:14:11 +0900

    05.28 페이지 로드 성공

--- daeut/src/main/java/com/daeut/daeut/controller/PageController.java
+++ daeut/src/main/java/com/daeut/daeut/controller/PageController.java
@@ -1,29 +1,10 @@
 package com.daeut.daeut.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/reservation")
 public class PageController {
-
-	// 결제 이동
-	@GetMapping("/payment")
-    public String payment() {
-        return "/reservation/payment";
-    }
-
-	// 결제 성공
-	@GetMapping("/paymentDone")
-    public String paymentDone() {
-        return "/reservation/paymentDone";
-    }
-
-	// 결제 실패
-    @GetMapping("/paymentFalse")
-    public String paymentFalse() {
-        return "/reservation/paymentFalse";
-    }
+    
 }
