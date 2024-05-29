a65fe232bd5bf877a9da97531c9984a32d59dccc
Joono0513 <tbc04156@gmail.com>
2024-05-29 12:55:02 +0900

    usercontroller 위치 이동

--- daeut/src/main/java/com/daeut/daeut/auth/controller/UserController.java
+++ daeut/src/main/java/com/daeut/daeut/auth/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.daeut.daeut.user.controller;
+package com.daeut.daeut.auth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
