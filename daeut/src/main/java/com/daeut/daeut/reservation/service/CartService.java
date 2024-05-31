e4c41cd18b2994144f724b6e8f2e5afdd0fbdace
JongsikLEE01 <137877490+JongsikLEE01@users.noreply.github.com>
2024-05-31 11:00:17 +0900

    정리

--- daeut/src/main/java/com/daeut/daeut/reservation/service/CartService.java
+++ daeut/src/main/java/com/daeut/daeut/reservation/service/CartService.java
@@ -7,6 +7,7 @@ import com.daeut.daeut.reservation.dto.Cart;
 public interface CartService {
     public List<Cart> cartList(int userNo) throws Exception;
     public int cartInsert(Cart cart) throws Exception;
+    public int cartUpdate(Cart cart) throws Exception;
     public int cartDelete(int cartNo) throws Exception;
     public int cartDeleteAll(int userNo) throws Exception;
 }
--- daeut/src/main/java/com/daeut/daeut/reservation/service/CartServiceImpl.java
+++ daeut/src/main/java/com/daeut/daeut/reservation/service/CartServiceImpl.java
@@ -8,38 +8,45 @@ import org.springframework.stereotype.Service;
 import com.daeut.daeut.reservation.dto.Cart;
 import com.daeut.daeut.reservation.mapper.CartMapper;
 
+
 @Service
 public class CartServiceImpl implements CartService{
 
     @Autowired
     private CartMapper cartMapper;
 
+    // 목록
     @Override
     public List<Cart> cartList(int userNo) throws Exception {
-        // 목록
         List<Cart> cartList = cartMapper.cartList(userNo);
         return cartList;
     }
 
+    // 삽입
     @Override
     public int cartInsert(Cart cart) throws Exception {
-        // 카트추가
         int result = cartMapper.cartInsert(cart);
         return result;
     }
 
+    // 수정
+    @Override
+    public int cartUpdate(Cart cart) throws Exception {
+        int result = cartMapper.cartUpdate(cart);
+        return result;
+    }
+
+    // 삭제
     @Override
     public int cartDelete(int cartNo) throws Exception {
-        // 선택삭제
         int result = cartMapper.cartDelete(cartNo);
         return result;
     }
 
+    // 모두 삭제
     @Override
     public int cartDeleteAll(int userNo) throws Exception {
-        // 전체삭제
         int result = cartMapper.cartDeleteAll(userNo);
         return result;
     }
-    
 }
