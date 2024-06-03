a56a1a36ec9f0517a20b41a9460a4f7348d6d274
djhwanndj <dodgehwang@gmail.com>
2024-06-03 18:20:19 +0900

    예약페이지 모아보기 고민 중

---
+++ daeut/SQL/리뷰샘플데이터.sql
@@ -0,0 +1,8 @@
+-- 파트너
+-- partner, 123456
+
+INSERT INTO review (review_title, review_content, review_rating, user_no, payment_no, partner_no)
+VALUES ('좋은 서비스였어요', '친절하고 깨끗한 곳이었습니다.', 5, 1, 12345, 3);
+
+INSERT INTO review (review_title, review_content, review_rating, user_no, payment_no, partner_no)
+VALUES ('잘 해주셨습니다.', '만족했어요', 4, 2, 12345, 1);
