453be2786c8725911e88081416f598863fb9fd34
djhwanndj <dodgehwang@gmail.com>
2024-06-05 16:35:39 +0900

    회원 탈퇴

--- daeut/SQL/DDL.sql
+++ daeut/SQL/DDL.sql
@@ -266,9 +266,12 @@ ALTER TABLE review
     REFERENCES payment (payment_no);
 
 ALTER TABLE partner
-  ADD CONSTRAINT FK_users_TO_partner
-    FOREIGN KEY (user_no)
-    REFERENCES users (user_no);
+DROP FOREIGN KEY FK_users_TO_partner; -- 이전의 외래 키 제약 조건 삭제
+
+ALTER TABLE partner
+ADD CONSTRAINT FK_users_TO_partner
+FOREIGN KEY (user_no) REFERENCES users(user_no)
+ON DELETE CASCADE;
 
 ALTER TABLE board
   ADD CONSTRAINT FK_users_TO_board
