768ef8a1d61576349ee42a7f77340916ab367f75
Joono0513 <tbc04156@gmail.com>
2024-06-07 10:57:37 +0900

    SQL

--- daeut/SQL/예약샘플데이터.sql
+++ daeut/SQL/예약샘플데이터.sql
@@ -8,7 +8,7 @@
 
 -- TRUNCATE TABLE service;
 INSERT INTO service (service_category, service_name, service_price, service_content, partner_no)
-VALUES ('청소','[테스트] 청소', 300, '테스트데이터, 청소 서비스 입니다.', 3);
+VALUES ('청소','[테스트] 청소', 300, '테스트데이터, 청소 서비스 입니다.', 1);
 
 -- reservation 샘플데이터(Order)
 INSERT INTO orders (user_no, partner_no, order_status, orders_no, total_quantity, total_price, total_count) 
