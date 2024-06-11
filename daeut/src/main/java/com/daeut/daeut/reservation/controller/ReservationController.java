5c3d7e97c11e2ad8d765175ea3f0c0b0c207fba9
djhwanndj <160221884+djhwanndj@users.noreply.github.com>
2024-06-11 23:27:35 +0900

    Update ReservationController.java

--- daeut/src/main/java/com/daeut/daeut/reservation/controller/ReservationController.java
+++ daeut/src/main/java/com/daeut/daeut/reservation/controller/ReservationController.java
@@ -253,4 +253,21 @@ public class ReservationController {
     }
 
 
+@PostMapping("/reviewDelete")
+public String reviewDelete(@RequestParam("reviewNo") int reviewNo) throws Exception {
+	int result = reviewService.reviewDelete(reviewNo);
+
+	if result = reveiwService.reviewDelete(reviewNo);
+
+	if (result == 0) {
+		log.info("리뷰 삭제 실패...")
+		return "redirect:/reservation/reservation"
+	}
+	 Files file = new Files();
+        file.setParentTable("reviewNo");
+        file.setParentNo(reviewNo);
+        fileService.deleteByParent(file);
+
+        log.info("리뷰 삭제 성공...");
+        return "redirect:/reservation/reservation";
 }
